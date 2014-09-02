0739bcba1b2cd709c250b66992e9a7e6017d8407
Robert Jackson <robert.w.jackson@me.com>
2014-09-02 14:41:29 -0400

    Provide helper function to register custom helpers.
    This makes it possible to unit test views a bit easier:
    
    ```javascript
    import registerWaypointHelpers from 'ember-waypoints/utils/register-helpers';
    
    moduleFor('view:foo-bar', 'view:foo-bar', {
      setup: function() {
        registerWaypointHelpers();
      }
    });
    ```

--- app/initializers/register-helper.js
+++ app/initializers/register-helper.js
@@ -1,12 +1,8 @@
 import Ember from 'ember';
-import WaypointComponent from 'ember-waypoints/components/waypoint';
+import registerHelpers from 'ember-waypoints/utils/register-helpers';
 
 export default {
   name: 'ember-waypoints-register-helper',
 
-  initialize: function() {
-    var helper = Ember.Handlebars.makeViewHelper(WaypointComponent);
-
-    Ember.Handlebars.registerHelper('waypoint', helper);
-  }
+  initialize: registerHelpers
 };
