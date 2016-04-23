061dcdf9948960da7f8404f70257c9d90ddebe64
Brian Cardarella <brian@dockyard.com>
2016-04-23 00:29:22 -0400

    Resolves #14 vendor-suffix deprecation warning
    Will inject waypoints.js wrapped with the fastboot fix into vendor.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var fs = require('fs');
 var path = require('path');
+var writeFile = require('broccoli-file-creator');
+var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-waypoints',
 
-  contentFor: function(type) {
-    if (type === 'vendor-suffix') {
-      return 'if (typeof(document) !== "undefined") {\n' + fs.readFileSync(path.join(this.project.bowerDirectory + '/jquery-waypoints/waypoints.js')) + '\n}';
-    }
+  treeForVendor: function(tree) {
+    var content = 'if (typeof(document) !== "undefined") {' +
+      fs.readFileSync(path.join(this.project.bowerDirectory + '/jquery-waypoints/waypoints.js')) +
+    '}';
+
+    var waypointsTree = writeFile('/waypoints.js', content);
+
+    return mergeTrees([tree, waypointsTree]);
+  },
+  included: function(app) {
+    this.app = app;
+    app.import('vendor/waypoints.js');
   }
 }
